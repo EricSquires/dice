edc446806a71565bf0a89569399cee890b82e426
Eric Squires <eric.h.squires@gmail.com>
2017-07-21 05:19:19 -0600

    Clean up JS, make the camera perspective and dice layout a bit more interesting

--- Dice/wwwroot/js/diceDrawer.js
+++ Dice/wwwroot/js/diceDrawer.js
@@ -16,6 +16,8 @@
         scene = buildScene();
 
         camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+        camera.position.set(0, -1, 0);
+        camera.lookAt(new THREE.Vector3(0, 0, -10));
 
         renderer = new THREE.WebGLRenderer();
 
@@ -34,17 +36,32 @@
         scene.add(new THREE.AmbientLight(0x404040));
 
         var light = new THREE.PointLight(0xffffff, 0.8);
-        light.position.set(-20, 20, 5);
+        light.position.set(-5, 5, 0);
         scene.add(light);
 
-        light = new THREE.PointLight(0xffffff, 0.8);
-        light.position.set(20, 20, 5);
+        light = new THREE.PointLight(0xffffff, 0.6);
+        light.position.set(5, -5, 0);
         scene.add(light);
 
         addBackground(scene);
+        
+        var numDice = dice.length;
+        var spacing = 2.5;
+        var size = 1;
+        var offset;
+
+        if (numDice % 2 === 0) {
+            offset = (size + spacing) / 2 + (numDice / 2 - 1) * (size + spacing);
+        }
+        else {
+            offset = size * Math.floor(numDice / 2) + spacing * Math.floor(numDice / 2);
+        }
+
+        var currentOffset = -offset;
 
         dice.map(function (d) {
-            addDie(scene, d);
+            addDie(scene, d, size, currentOffset);
+            currentOffset += spacing + size;
         });
 
         return scene;
@@ -60,13 +77,14 @@
         scene.add(plane);
     }
 
-    function addDie(scene, value) {
-        var geometry = new THREE.BoxGeometry(1, 1, 1);
+    function addDie(scene, value, size, offset) {
+
+        var geometry = new THREE.BoxGeometry(size, size, size);
         var material = new THREE.MeshPhongMaterial({ color: 0xaa0000 });
         var mesh = new THREE.Mesh(geometry, material);
 
         mesh.translateZ(-5);
-        mesh.translateX(-2.5 + 5 * diceMesh.length);
+        mesh.translateX(offset);
         diceMesh.push(mesh);
 
         scene.add(mesh);
--- Dice/wwwroot/js/site.js
+++ Dice/wwwroot/js/site.js
@@ -10,11 +10,12 @@
 roll = function () {
     var dice = $(".dice");
 
-    dice.each(function (index, die) {
-        var numFaces = parseInt($(die).children(".die-faces").text());
+    dice.each(function () {
+        var die = $(this);
+        var numFaces = parseInt(die.find(".die-faces").text());
 
         $.get("/api/Roll/" + numFaces, function (response) {
-            dice.children(".die-value:eq(" + index + ")").text(response);
+            die.find(".die-value").text(response);
         });
     });
 };
